11fad67814f409d759c174e3fe6f4038e7146fb6
Peter Kingori <kpndegwa@gmail.com>
2019-05-02 17:06:10 +0300

    updated solution by removing unnecessary variable

--- vowelchecker.js
+++ vowelchecker.js
@@ -7,7 +7,6 @@ array.forEach(function vowel_check(vowel)
   var i = -1;
   while(i<array.length){
   i = i+1;
-  vowel =array[i];
 		if((array[i]==="a")||(array[i]==="e")||				(array[i]==="i")||(array[i]==="o")||(array[i]==="u"))
     {
     	array[i]="-";
